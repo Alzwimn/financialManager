546fb8aab7ba18e1d768f930cac352b5be16f26b
Alzwimn <alzwimn@gmail.com>
2020-08-12 18:36:02 -0300

    initial setup

---
+++ test/jest.test.js
@@ -0,0 +1,8 @@
+test( "Devo conhecer as principais acertivas do JEST", () => {
+	let number = null
+	expect(number).toBeNull()
+})
+
+test("trabalha com objetos", () => {
+	const obj = { name: "John", mail: "john@mail.com"}
+})
