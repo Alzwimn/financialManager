c39da0ed3aa285162c9401449b343a4141c763ed
Alzwimn <alzwimn@gmail.com>
2020-09-07 12:42:58 -0300

    Refatorando tratamento de erros

--- src/services/user.js
+++ src/services/user.js
@@ -1,14 +1,16 @@
+const ValidationError = require("../errors/ValidationError")
+
 module.exports = (app) => {
     const findAll = (filter = {}) => {
         return app.db("users").where(filter).select();
     }
     const save = async (user) => {
-        if(!user.name) return { error: "Nome é um atributo obrigatório"}
-        if(!user.mail) return { error: "Email é um atributo obrigatório"}
-        if(!user.password) return { error: "Senha é um atributo obrigatório"}
+        if(!user.name) throw new ValidationError("Nome é um atributo obrigatório")
+        if(!user.mail) throw new ValidationError("Email é um atributo obrigatório")
+        if(!user.password) throw new ValidationError("Senha é um atributo obrigatório")
 
         const userDB = await findAll({ mail:user.mail })
-        if(userDB && userDB.length > 0) return {error: "Já exisite um usuário com esse email"} 
+        if(userDB && userDB.length > 0) throw new ValidationError("Já exisite um usuário com esse email") 
 
         return app.db("users").insert(user, "*")
     }
