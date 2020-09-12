ca9d5044d339d2737c6c1d1b016af1b910c52089
Alzwimn <alzwimn@gmail.com>
2020-09-11 23:01:36 -0300

    Adicionado passport para proteger rotas

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
         .post(app.routes.auth.signin)        
 
     app.route("/users")
+        .all(app.config.passport.authenticate())
         .get(app.routes.users.findAll)
         .post(app.routes.users.create)
 
