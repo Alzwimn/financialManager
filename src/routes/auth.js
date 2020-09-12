7f3c088404f532d7221982ebd9929fb640c1b327
Alzwimn <alzwimn@gmail.com>
2020-09-12 14:48:56 -0300

    Redefinição e aprimoracao de rotas

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,12 @@
+const express = require("express")
 const jwt = require("jwt-simple")
 const bycrpt = require("bcrypt-nodejs")
 const secret = "Segredo";
 const ValidationError = require("../errors/ValidationError")
 
 module.exports = (app) => {
-
-    const signin = (req, res, next) => {
+    const router = express.Router()
+    router.post("/signin", (req, res, next) => {
         app.services.user.findOne({mail: req.body.mail})
         .then(user => {
             
@@ -21,6 +22,16 @@ module.exports = (app) => {
                 res.status(200).json({token})
             } else { throw new ValidationError("Usuário ou senha inválido") }
         }).catch(err => next(err))
-    }
-    return {signin}
+    })
+
+    router.post( "/signup", async (req, res, next) => {
+        try {
+            const result = await app.services.user.save(req.body)
+            return res.status(201).json(result[0])
+        } catch (err) {
+          return next(err)
+        }
+    })
+     
+    return router
 }
